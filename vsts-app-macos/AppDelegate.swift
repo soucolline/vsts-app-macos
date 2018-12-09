8da3fbcb789109620bd6141f580a31c541491ca1
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2018-12-09 12:13:35 +0100

    feat(master): initial commit

---
+++ vsts-app-macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vsts-app-macos
+//
+//  Created by Zlatan on 09/12/2018.
+//  Copyright © 2018 Zlatan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ vsts-app-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vsts-app-macos
+//
+//  Created by Zlatan on 09/12/2018.
+//  Copyright © 2018 Zlatan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
